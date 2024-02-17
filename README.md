85848eec419a0cde853a10f38513d706c1987784
suwibaki099 <jandy.gulpe9@gmail.com>
2024-02-18 00:06:56 +0800

    first release

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# password-checker
+# Password-checker version 0.1.1
+
+first configure your python interpreter... Python3
+if you dont have one download it here https://www.python.org/downloads/
+
+compatibility:
+windowds
+python3. >= python3
+
+# Description
+
+## this is a password manager that will check if your passwords has been leak in the internet world.
+
+### how to use!
+first you need to import this command to run the program.
+
+### commands:
+we need to import this external library to get and read api's : "pip install requests" after that
+in cmd run this code: "checkmypass.py p.txt"
+
+## note!
+
+you need to put your password in p.txt to maintain the anominity of your password. It will also become your password storage.
+you can put this program in your flashdrive if you want.
+
+
+this all the information... 
+
+Open source.
---
+++ checkmypass.py
@@ -0,0 +1,59 @@
+import requests
+import hashlib
+import sys
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise RuntimeError(f'Error fetching: {response.status_code}, check your url and start again')
+    return response
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail_char = sha1password[:5], sha1password[5:]
+    pwned_response = request_api_data(first5_char)
+    return get_password_leaks_count(pwned_response, tail_char)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times... you should change your password')
+        else:
+            print(f'{password} was NOT found. Carry on!')
+    return 'done!'
+
+
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    with open(file_name) as file:
+        passwords = file.readlines()
+        for p in passwords:
+            read = p.split()
+            ext = main(read)
+        sys.exit(ext)
+
+"""
+split()
+splitlines()
+
+study this!!
+
+backlog:
+        to make this secure i need to create a 
+        file or note accepter in this code so that i can 
+        safety check my password if its been hack.
+        because this might not still secured
+"""
